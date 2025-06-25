37d84c70fc9aeb51622e761989e3b0c03f7f7f2e
Morshedkhondokar <morshedkhondokar99@gmail.com>
2025-06-25 16:18:46 +0600

    add blogs and blog component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 
 import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Bookmarks/Bookmarks'
+import Header from './Components/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-2xl bg-orange-400'>Hello</h1>
+      <Header></Header>
+      <div className='md:flex'>
+         <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
